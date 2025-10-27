dad4a57caa5873f125f057c36e685a49925a556c
BulandaK <eskimosl@wp.pl>
2025-10-27 13:00:06 +0100

    feat(account hook): connect type definition

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,32 +1,44 @@
-import { CryptoHookFactory } from "@/types/hook";
-import useSWR from "swr";
+import { CryptoHookFactory } from '@/types/hook';
+import useSWR from 'swr';
 
+type UseAccountResponse = {
+  connect: () => void;
+};
 
-type AccountHookFactory = CryptoHookFactory<string>;
+type AccountHookFactory = CryptoHookFactory<string, UseAccountResponse>;
 
+export type UseAccountHook = ReturnType<AccountHookFactory>;
 
+export const hookFactory: AccountHookFactory =
+  ({ provider, ethereum }) =>
+  () => {
+    const swrRes = useSWR(
+      provider ? 'web3/useAccount' : null,
+      async () => {
+        const accounts = await provider!.listAccounts();
+        const account = accounts[0].address;
 
-export type UseAccountHook = ReturnType<AccountHookFactory>
-
-export const hookFactory: AccountHookFactory = ({provider}) =>()=>{
-    const swrRes =useSWR(
-        provider? "web3/useAccount" : null,
-        async () =>{
-            const accounts = await provider!.listAccounts()
-            const account = accounts[0].address;
-
-            if(!account){
-                throw "Cannot retreive account! Please, connect to web3 wallet."
-            }
-            return account;
-        },
-        {
-            revalidateOnFocus:false
+        if (!account) {
+          throw 'Cannot retreive account! Please, connect to web3 wallet.';
         }
-    )
+        return account;
+      },
+      {
+        revalidateOnFocus: false,
+      }
+    );
 
-    return swrRes
-}
+    const connect = async () => {
+      try {
+        console.log('wykonuje sie connect');
+        ethereum?.request({ method: 'eth_requestAccounts' });
+      } catch (e) {
+        console.log(e);
+      }
+    };
 
-
-export const useAccount = hookFactory({ethereum: undefined,provider:undefined});
+    return {
+      ...swrRes,
+      connect,
+    };
+  };
--- types/hook.ts
+++ types/hook.ts
@@ -10,17 +10,17 @@ export type Web3Dependencies = {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type CryptoHookFactory<D = any, P = any> = {
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook;
+export type CryptoHookFactory<D = any,R=any, P = any> = {
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D,R,P>;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type CryptoHandlerHook<D = any, P = any> = (
+export type CryptoHandlerHook<D = any,R=any, P = any> = (
   params?: P
-) => CryptoSWRResponse<D>;
+) => CryptoSWRResponse<D,R>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type CryptoSWRResponse<D = any> = SWRResponse<D>;
+export type CryptoSWRResponse<D = any, R = any> = SWRResponse<D> & R;
 
 // export type CryptoHookFactory<D = any,P = any> = {
 //   (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>;
