b2e7b34463c3f0784e30e795f3c5f633682c7cad
BulandaK <eskimosl@wp.pl>
2025-11-06 16:00:27 +0100

    feat(mint token): listing price test

--- test/NftMarket.test.js
+++ test/NftMarket.test.js
@@ -1,8 +1,10 @@
 
 const NftMarket = artifacts.require("NftMarket");
 
+
 contract("NftMarket", accounts => {
   let _contract = null;
+  let _nftPrice = web3.utils.toWei("0.3", "ether");
 
   before(async () => {
     _contract = await NftMarket.deployed();
@@ -11,7 +13,7 @@ contract("NftMarket", accounts => {
   describe("Mint token", () => {
     const tokenURI = "https://test.com";
     before(async () => {
-      await _contract.mintToken(tokenURI, {
+      await _contract.mintToken(tokenURI, _nftPrice,  {
         from: accounts[0]
       })
     })
@@ -20,22 +22,26 @@ contract("NftMarket", accounts => {
       const owner = await _contract.ownerOf(1);
       assert.equal(owner, accounts[0], "Owner of token is not matching address[0]");
     })
-    
+
     it("first token should point to the correct tokenURI", async () => {
       const actualTokenURI = await _contract.tokenURI(1);
 
       assert.equal(actualTokenURI, tokenURI, "tokenURI is not correctly set");
     })
-    
+
     it("should not be possible to create a NFT with used tokenURI", async () => {
       try {
-        await _contract.mintToken(tokenURI, {
+        await _contract.mintToken(tokenURI, _nftPrice, {
           from: accounts[0]
         })
       } catch(error) {
         assert(error, "NFT was minted with previously used tokenURI");
       }
     })
-  })
 
+    it("should have one listed item", async () => {
+      const listedItemCount = await _contract.listedItemsCount();
+      assert.equal(listedItemCount.toNumber(), 1, "Listed items count is not 1");
+    })
+  })
 })
